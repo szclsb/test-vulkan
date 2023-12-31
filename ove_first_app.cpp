ad454156aca4111f6ee9262dba8c0225642342e6
Claudio Seitz <szclsb@gmail.com>
2023-12-31 15:45:45 +0100

    Vulkan Tutorial 9: impl push constants

--- ove_first_app.cpp
+++ ove_first_app.cpp
@@ -1,9 +1,19 @@
 #include "ove_first_app.h"
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include "GLM/glm/glm.hpp"
+
 #include <stdexcept>
 #include <array>
 
 namespace ove {
+    // https://docs.vulkan.org/guide/latest/shader_memory_layout.html#alignment-requirements
+    struct SimplePushConstantData {
+        glm::vec2 offset;
+        alignas(16) glm::vec3 color;
+    };
+
     FirstApp::FirstApp() {
         loadModels();
         createPipelineLayout();
@@ -35,12 +45,17 @@ namespace ove {
     }
 
     void FirstApp::createPipelineLayout() {
+        VkPushConstantRange pushConstantRange;
+        pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+        pushConstantRange.offset = 0;
+        pushConstantRange.size = sizeof(SimplePushConstantData);
+
         VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
         pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
         pipelineLayoutInfo.setLayoutCount = 0;
         pipelineLayoutInfo.pSetLayouts = nullptr;
-        pipelineLayoutInfo.pushConstantRangeCount = 0;
-        pipelineLayoutInfo.pPushConstantRanges = nullptr;
+        pipelineLayoutInfo.pushConstantRangeCount = 1;
+        pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
         if (vkCreatePipelineLayout(oveDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
             throw std::runtime_error("failed to create pipeline layout!");
         }
@@ -123,7 +138,20 @@ namespace ove {
 
         ovePipeline->bind(commandBuffers[imageIndex]);
         oveModel->bind(commandBuffers[imageIndex]);
-        oveModel->draw(commandBuffers[imageIndex]);
+
+        for (int j = 0; j < 4; j++) {
+            SimplePushConstantData push{};
+            push.offset = {0.0f, -0.4f + j * 0.25f};
+            push.color = {0.0f, 0.0f, 0.2f + j * 0.2f};
+
+            vkCmdPushConstants(
+                    commandBuffers[imageIndex],
+                    pipelineLayout,
+                    VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
+                    0, sizeof(SimplePushConstantData),
+                    &push);
+            oveModel->draw(commandBuffers[imageIndex]);
+        }
 
         vkCmdEndRenderPass(commandBuffers[imageIndex]);
         if (vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS) {
